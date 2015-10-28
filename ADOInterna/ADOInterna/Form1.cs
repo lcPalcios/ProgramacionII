67e69387cadc600da78823679321fe4f608472b0
lcPalcios <dalex_31@hotmail.com>
2015-10-27 18:29:03 -0600

    Clase 27-10

--- ADOInterna/ADOInterna/Form1.cs
+++ ADOInterna/ADOInterna/Form1.cs
@@ -7,14 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MetroFramework.Forms;
+
 
 namespace ADOInterna
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MetroForm
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ ADOInterna/Entidad/Estudiante.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    public class Estudiante
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNac { get; set; }
+        public string Telefono { get; set; }
+
+        public Estudiante() { }
+
+        public Estudiante(string pNombre, string pApellido, DateTime pFecha,
+                            string pTelefono)
+        {
+            Nombre = pNombre;
+            Apellido = pApellido;
+            FechaNac = pFecha;
+            Telefono = pTelefono;
+        }
+
+        public Estudiante(int pId, string pNombre, string pApellido, 
+            DateTime pFecha, string pTelefono)
+        {
+            Id = pId;
+            Nombre = pNombre;
+            Apellido = pApellido;
+            FechaNac = pFecha;
+            Telefono = pTelefono;
+        }
+
+
+
+    }
+}
